1c0ec5f695dc872f7d97a95929de894f6ce6f2ae
saaiiix22 <sai.naivedya123@gmail.com>
2025-09-03 11:27:47 +0530

    changes

--- frontend/src/components/eachReviewStarWise/EachReviewStarWise.js
+++ frontend/src/components/eachReviewStarWise/EachReviewStarWise.js
@@ -1,52 +1,61 @@
 "use client";
-import Box from '@mui/material/Box';
-import Tab from '@mui/material/Tab';
-import TabContext from '@mui/lab/TabContext';
-import TabList from '@mui/lab/TabList';
-import TabPanel from '@mui/lab/TabPanel';
-import { useState } from 'react';
-import CustomerReviewCard from '../customerReviewCard/CustomerReviewCard';
-export default function EachReviewStarWise() {
-    const [value, setValue] = useState('5');
+import styles from "./eachReviewStarWise.module.css";
+import Box from "@mui/material/Box";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import { useState } from "react";
+import CustomerReviewCard from "../customerReviewCard/CustomerReviewCard";
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
-    return (
-        <div>
-            <Box sx={{ width: '100%', typography: 'body1' }}>
-                <TabContext value={value}>
-                    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                        <TabList onChange={handleChange} aria-label="lab API tabs example">
-                            <Tab label="5 Star Reviews" value="5" />
-                            <Tab label="4 Star Reviews" value="4" />
-                            <Tab label="3 Star Reviews" value="3" />
-                            <Tab label="2 Star Reviews" value="2" />
-                            <Tab label="1 Star Reviews" value="1" />
-                        </TabList>
-                    </Box>
-                    <TabPanel value="1"></TabPanel>
-                    <TabPanel value="2">Item Two</TabPanel>
-                    <TabPanel value="3">Item Three</TabPanel>
-                    <TabPanel value="4">Item Four</TabPanel>
-                    <TabPanel value="5">
-                        <div className='grid grid-cols-12 gap-5'>
-                            <div className='col-span-6'>
-                                <CustomerReviewCard/>
-                            </div>
-                            <div className='col-span-6'>
-                                <CustomerReviewCard/>
-                            </div>
-                            <div className='col-span-6'>
-                                <CustomerReviewCard/>
-                            </div>
-                            <div className='col-span-6'>
-                                <CustomerReviewCard/>
-                            </div>
-                        </div>
-                    </TabPanel>
-                </TabContext>
-            </Box>
-        </div>
-    );
+export default function EachReviewStarWise() {
+  const [value, setValue] = useState("5");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div>
+      <Box sx={{ width: "100%", typography: "body1" }}>
+        <TabContext value={value}>
+          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+            {/* 👇 Only one centralised class here */}
+            <TabList
+              onChange={handleChange}
+              aria-label="review tabs"
+              className={styles.tabList}
+            >
+              <Tab label="5 Star Reviews" value="5" />
+              <Tab label="4 Star Reviews" value="4" />
+              <Tab label="3 Star Reviews" value="3" />
+              <Tab label="2 Star Reviews" value="2" />
+              <Tab label="1 Star Reviews" value="1" />
+            </TabList>
+          </Box>
+
+          <TabPanel value="1"></TabPanel>
+          <TabPanel value="2">Item Two</TabPanel>
+          <TabPanel value="3">Item Three</TabPanel>
+          <TabPanel value="4">Item Four</TabPanel>
+          <TabPanel value="5">
+            <div className="grid grid-cols-12 gap-5">
+              <div className="col-span-6">
+                <CustomerReviewCard />
+              </div>
+              <div className="col-span-6">
+                <CustomerReviewCard />
+              </div>
+              <div className="col-span-6">
+                <CustomerReviewCard />
+              </div>
+              <div className="col-span-6">
+                <CustomerReviewCard />
+              </div>
+            </div>
+          </TabPanel>
+        </TabContext>
+      </Box>
+    </div>
+  );
 }
